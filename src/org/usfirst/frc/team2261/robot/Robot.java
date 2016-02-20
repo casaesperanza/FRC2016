2fcd441c267c1c946c7f29f8cf6aac23d7eb2d8a
Casa De La Esperanza <mlozano@bouldercounty.org>
2016-02-20 13:26:40 -0700

    Changed a major amount of stuff. Made sure that wheels drove foward, joystick for lowering and raising, modded constants, changed kicker constants.

--- src/org/usfirst/frc/team2261/robot/Robot.java
+++ src/org/usfirst/frc/team2261/robot/Robot.java
@@ -7,6 +7,8 @@ package org.usfirst.frc.team2261.robot;
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
+
+// CLEAN IF YOU CANNOT CONNECT TO THE ROBORIO. HOW TO CLEAN: Go to Project > Clean to clean up project; may be able to work fine after. 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
@@ -30,39 +32,48 @@ public class Robot extends SampleRobot {
     static double MOTOR_EXPIRATION = UPDATE_DELAY * 4;
     
     // Dampening constants
-	private static final double kPWM = 0;
-	private static final double kPWM_MAX = 1;
-	private static final double kdPWM_MAX = 0.1;
+    //Not used until we have ability to do so.
+	private static final double kPWM = 0.005; //added .005 to 0; hopefully able to have increased driving ability.
+	private static final double kPWM_MAX = 0.1; //dropped 1 down to 0.1; made the driving speed have less turning radius.
+	private static final double kdPWM_MAX = 0.1; 
     
     // IMPORTANT!!!!!!
-    // Set to true for driving forklift from accesory y-axis (when not at limit or being driven by accesory buttons)
+    // Set to true for driving fork_lift from accessory y-axis (when not at limit or being driven by accessory buttons)
     static boolean MAP_YAXIS_TO_FORKLIFT = true;
+    static boolean MAP_ZAXIS_TO_ClAW  = true;
     
-    // Values for forklift motor (determined by testing)
+    // Values for fork_lift motor (determined by testing)
     static double FORKLIFT_NOMINAL_UP = 0.2;
     static double FORKLIFT_ALOT_UP = 0.2;
     static double FORKLIFT_FULL_LIFT = 0.75;
     static double FORKLIFT_NOMINAL_DOWN = -0.5;
+    static double CLAW_OPEN = 0.5;
+    static double CLAW_CLOSE = -0.5;
+    static double KICK_DOWN = -0.9;//ARM Added much more added effect to Kicker; originally -.01 and added to -.09.
+    static double KICK_UP = 0.9;//DISARM Added much more added effect to Kicker; originally .01 and added to .09.
     
     // Create a new RobotDrive, composed of the following motor controllers
-	Talon rightController = new Talon(2);
-	Talon leftController = new Talon(3);
+	Talon rightController = new Talon(2); //Undo'd the Talon.
+	Talon leftController = new Talon(3); //Undo'd the Talon.
 	RobotDrive robotDrive = new RobotDrive(leftController, rightController);
 
 	Spark clawController= new Spark(5);
 	
 	Spark upAndDownController= new Spark(6);
 	
-	Spark kickerController= new Spark(7);
+	Spark kickerController= new Spark(7); //Sparks number 7 and number 8 are shared.
 
-    // Create new Joystick on ports 1 and 2
+    // Create new Joy_stick on ports 1 and 2
     Joystick driverJoystick = new Joystick(0);
     Joystick accessoryJoystick = new Joystick(1);
     static double prevPWM, dPWM = 0;
     
-    // Forklift related
-      DigitalInput forkliftLowerSwitch = new DigitalInput(2);
-    DigitalInput forkliftUpperSwitch = new DigitalInput(3);
+    // Kicker_Claw Sensor
+    //DigitalInput forkliftLowerSwitch = new DigitalInput(2);
+    //DigitalInput forkliftUpperSwitch = new DigitalInput(3);
+    DigitalInput kickerSwitch = new DigitalInput(2);
+    DigitalInput forkliftSwitch = new DigitalInput(3);
+  
     
     boolean robotInitted = false;
     boolean useForklift = false;
@@ -111,11 +122,12 @@ public class Robot extends SampleRobot {
         while(isOperatorControl() && isEnabled()) {
           
             // Drive the Robot
-            robotDrive.arcadeDrive(scaleJoystickInput(driverJoystick.getY())/2,  // Y=Forward and Backward Drive
-                                   scaleJoystickInput(driverJoystick.getZ())/2); // Z= Rotate Clockwise and Counter-Clockwise
+        	//Changed 2 to -2 on division for both drives
+            robotDrive.arcadeDrive(scaleJoystickInput(driverJoystick.getY())/-1.4123,  // Y=Forward and Backward Drive (Divided by -4 instead of -2 to ensure speed WAS lost during process.)
+                                   scaleJoystickInput(driverJoystick.getZ())/-1.4123); // Z= Rotate Clockwise and Counter-Clockwise (Divided by 4 instead of 1 to ensure speed WAS lost during process.)
             
-            // Forklift Operation
-            if(isForkliftAtLowerLimit()) { // Trying to go down and not at lower limit
+            // Fork_lift Operation
+          /*  if(isForkliftAtLowerLimit()) { // Trying to go down and not at lower limit
             	upAndDownController.set(Robot.FORKLIFT_NOMINAL_UP);
             } else if(isForkliftAtUpperLimit()) { // While trying to go up and not at the upper limit
             	upAndDownController.set(Robot.FORKLIFT_NOMINAL_DOWN);
@@ -134,7 +146,44 @@ public class Robot extends SampleRobot {
             	} else {
                 	upAndDownController.set(0); // When not testing,f no button is pressed, stop the motor
             	}
+            }*/
+  
+              if(accessoryJoystick.getRawButton(4)) { // Y button
+            	upAndDownController.set(Robot.FORKLIFT_FULL_LIFT);
+            } else if(accessoryJoystick.getRawButton(2)) { // A button
+            	upAndDownController.set(Robot.FORKLIFT_NOMINAL_DOWN);
+            	
+            }else {
+            	if(Robot.MAP_YAXIS_TO_FORKLIFT) {
+            		upAndDownController.set(-accessoryJoystick.getY()); // For testing, map the accessory 
+            										// joystick y-axis directly to the forklift motor controller
+            	} else {
+                	upAndDownController.set(0); // When not testing,f no button is pressed, stop the motor
+            	}
+            } 
+              
+              //Known that claw works; reversing coding and also added Z axis for claw as well.
+            if(accessoryJoystick.getRawButton(1)) { // X Button
+            	clawController.set(Robot.CLAW_OPEN);
+            } else if(accessoryJoystick.getRawButton(3)) { // B button
+            	clawController.set(Robot.CLAW_CLOSE);	
+            } else {
+            	if(Robot.MAP_ZAXIS_TO_ClAW) {
+            		clawController.set(-accessoryJoystick.getZ()); // For testing, map the accessory 
+            										// joystick z-axis directly to the forklift motor controller
+            	} else {
+                	clawController.set(0); // When not testing,f no button is pressed, stop the motor
+            	}
+            } 
+            
+            if(accessoryJoystick.getRawButton(8)) { // RT button
+            	kickerController.set(Robot.KICK_DOWN);
+            } else if(accessoryJoystick.getRawButton(7)) { // LT button
+            	kickerController.set(Robot.KICK_UP);	
             }
+            else {
+            	kickerController.set(0); // When not testing,f no button is pressed, stop the motor
+        	}
 
             // Print debug info
             putSmartDashboardValues();
@@ -180,13 +229,14 @@ public class Robot extends SampleRobot {
         upAndDownController.set(0);
     }
     
-    protected boolean isForkliftAtLowerLimit() {
-    	return !forkliftLowerSwitch.get();
+    protected boolean isKickerLoaded() {
+    	return !kickerSwitch.get();
     }
     
     protected boolean isForkliftAtUpperLimit() {
-    	return !forkliftUpperSwitch.get();
+    	return !forkliftSwitch.get();
     }
+    
     
     
     /**
@@ -214,10 +264,11 @@ public class Robot extends SampleRobot {
 	private void putSmartDashboardValues() {
 		/*
     	SmartDashboard.putNumber("accessoryJoystick.getY (forklift)", accessoryJoystick.getY());
-    	SmartDashboard.putBoolean("isForkliftAtLowerLimit()", isForkliftAtLowerLimit());
+    	SmartDashboard.putBoolean("isForkliftAtUpperLimit()", isForkliftAtLowerLimit());*/
+    	/*SmartDashboard.putBoolean("isKickerLoaded()", isKickerLoaded());
     	SmartDashboard.putBoolean("isForkliftAtUpperLimit()", isForkliftAtUpperLimit());
     	SmartDashboard.putNumber("upAndDownController.getRaw()", upAndDownController.getRaw());
-    	*/
+    	
 		SmartDashboard.putNumber("driverJoystick.getX()", driverJoystick.getX());
 		SmartDashboard.putNumber("scaleJoystickInput(driverJoystick.getX())",
 		                            scaleJoystickInput(driverJoystick.getX()));
@@ -236,7 +287,8 @@ public class Robot extends SampleRobot {
 		SmartDashboard.putNumber("accessoryJoystick.getThrottle()", accessoryJoystick.getThrottle());
 		SmartDashboard.putNumber("accessoryJoystick.getTwist()",accessoryJoystick.getTwist());
 		SmartDashboard.putBoolean("accessoryJoystick.getRawButton(2)",accessoryJoystick.getRawButton(2));
-		SmartDashboard.putBoolean("accessoryJoystick.getRawButton(3)",accessoryJoystick.getRawButton(3));
+		SmartDashboard.putBoolean("accessoryJoystick.getRawButton(3)",accessoryJoystick.getRawButton(3));*/
+		SmartDashboard.putNumber("accessoryJoystick.getPOV(0)", accessoryJoystick.getPOV(0)); //Added Point of View addition for controller.
 	}
 	
 }
